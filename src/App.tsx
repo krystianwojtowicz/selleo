829f0fe09888f9b389a2ae1531bd6484e2ebb8e1
Krystian <ipro.krystian.w@gmail.com>
2024-03-19 15:44:14 +0100

    searchbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import LiveWriting from './components/LiveWriting';
 import './index.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LiveWriting from './components/LiveWriting';
+import Home from './components/Home';
+import Searchbox from './components/Searchbox';
 
 function App() {
   return (
-    <div>
-      <LiveWriting />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul className="flex gap-[10px] ml-[10px] mt-[10px]">
+            <li>
+              <Link to="/livewriting">LiveWriting</Link>
+            </li>
+            <li>
+              <Link to="/searchbox">SearchBox</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/livewriting" Component={LiveWriting} />
+          <Route path="/searchbox" Component={Searchbox} />
+          <Route path="/" Component={Home} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
